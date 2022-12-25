f1b21b2b7f5a945faaa8da15974c6b5d0392b232
Rid Hafiz <ridzihafiz@gmail.com>
2022-12-25 16:51:45 +0700

    adding pagination

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -24,9 +24,27 @@ export const user_create = async (req = request, res = response) => {
 // read user
 export const user_read = async (req = request, res = response) => {
   try {
-    const result = await db.users.findMany();
+    // let page = 2;
+    // let limit = 10;
+    let { page = 1, limit = 10 } = req.query; // string
+    let skip = (page - 1) * limit;
+
+    const result = await db.users.findMany({
+      take: parseInt(limit),
+      skip: skip,
+    });
+
+    // informasi total data keseluruhan
+    const resultCount = await db.users.count(); //integer jumlah total data users
+
+    // generated total page
+    const totalPage = Math.ceil(resultCount / limit);
+
     return res.status(200).json({
       success: true,
+      currentPage: page - 0, // - 0 ubah dari String ke Int
+      totalPage: totalPage,
+      totalData: resultCount,
       data: result,
     });
   } catch (error) {
